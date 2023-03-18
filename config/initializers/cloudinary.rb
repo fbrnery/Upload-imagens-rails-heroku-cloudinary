be3433c67fc2135c342bfdb99a06c754196745a1
Fabricio Miranda <fbrnery@gmail.com>
2023-03-18 10:00:57 -0300

    Atualização

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,1 +1,6 @@
-
+Cloudinary.config do |config|
+  config.cloud_name = 'dkpiau8f9'
+  config.api_key = '337439994531178'
+  config.api_secret = '2CbVskfjBvGklXnm68pBJgCB22w'
+  config.secure = true
+end
